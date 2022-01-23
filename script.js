40dbfcbd16bcde6819fd961a3f24abd0e2a223a1
Kalinho <karls@hotmail.se>
2022-01-23 22:54:44 +0100

    changes to navbar functionality

--- script.js
+++ script.js
@@ -13,3 +13,17 @@ function changeLogo() {
 }
 
 window.addEventListener("load", changeLogo);
+
+const toggleButton = document.getElementsByClassName('toggle-menu')[0];
+const navLinks = document.getElementsByClassName('nav-links')[0];
+
+toggleButton.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+})
+
+navLinks.addEventListener('click', () => {
+    if (navLinks.classList[1]) {
+        navLinks.classList.toggle('active');
+    }
+    
+})
